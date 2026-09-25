e5ba2b7eac85d49b430aad310745b0dbda3d5059
Shailendra dwivedi <shailendrad765@gmail.com>
2026-09-25 15:11:25 +0530

    Phase 1 foundation: add lib/types.ts

---
+++ lib/types.ts
@@ -0,0 +1,35 @@
+export type PlayerColor = "red" | "blue" | "green" | "yellow";
+export type GameStatus = "waiting" | "playing" | "finished";
+
+export type Token = {
+  id: number;
+  steps: number;
+};
+
+export type Player = {
+  id: number;
+  name: string;
+  color: PlayerColor;
+  tokens: Token[];
+  connected: boolean;
+};
+
+export type GameConfig = {
+  playerCount: 2 | 3 | 4;
+  turnTimeSeconds: number;
+  requireSixToStart: boolean;
+  rollAgainOnSix: boolean;
+};
+
+export type GameState = {
+  id: string;
+  status: GameStatus;
+  config: GameConfig;
+  players: Player[];
+  currentPlayerIndex: number;
+  dice: number | null;
+  turnNumber: number;
+  winnerId: number | null;
+  message: string;
+  stateVersion: number;
+};
